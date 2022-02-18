8630b4292f8f30266438f4d8c6184c5ec0769815
Piyush Bagad <piyushnbagad11@gmail.com>
2022-02-18 22:14:35 +0100

    Minor

--- SlowFastProposalClassifier/diva_run_models.sh
+++ SlowFastProposalClassifier/diva_run_models.sh
@@ -4,14 +4,13 @@ REPO=/home/pbagad/projects/epic-kitchens-ssl/
 conda activate slowfast
 export PYTHONPATH=$REPO
 
+# echo "R2Plus1D"
+# cfg=$REPO/configs/EPIC-KITCHENS/R2PLUS1D/32x112x112_R18_K400_LR0.0025.yaml
+# bash diva_eval_action_detection.sh -c $cfg
 
-echo "R2Plus1D"
-cfg=$REPO/configs/EPIC-KITCHENS/R2PLUS1D/32x112x112_R18_K400_LR0.0025.yaml
-bash diva_eval_action_detection.sh -c $cfg
-
-echo "AVID-CMA"
-cfg=$REP/configs/AVID_CMA/32x112x112_R18_K400_LR0.0025.yaml
-bash diva_eval_action_detection.sh -c $cfg
+# echo "AVID-CMA"
+# cfg=$REP/configs/AVID_CMA/32x112x112_R18_K400_LR0.0025.yaml
+# bash diva_eval_action_detection.sh -c $cfg
 
 echo "CTP"
 cfg=$REPO/configs/EPIC-KITCHENS/CTP/32x112x112_R2+1D-18_K400_LR0.0025.yaml

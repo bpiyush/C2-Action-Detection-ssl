00a03fd37d49fb5551d720fe94df2186d6e3f034
Piyush Bagad <piyushnbagad11@gmail.com>
2022-02-19 08:01:34 +0100

    Minor

--- SlowFastProposalClassifier/diva_run_models.sh
+++ SlowFastProposalClassifier/diva_run_models.sh
@@ -12,13 +12,13 @@ export PYTHONPATH=$REPO
 # cfg=$REP/configs/AVID_CMA/32x112x112_R18_K400_LR0.0025.yaml
 # bash diva_eval_action_detection.sh -c $cfg
 
-echo "CTP"
-cfg=$REPO/configs/EPIC-KITCHENS/CTP/32x112x112_R2+1D-18_K400_LR0.0025.yaml
-bash diva_eval_action_detection.sh -c $cfg
+# echo "CTP"
+# cfg=$REPO/configs/EPIC-KITCHENS/CTP/32x112x112_R2+1D-18_K400_LR0.0025.yaml
+# bash diva_eval_action_detection.sh -c $cfg
 
-echo "RSPNet"
-cfg=$REPO/configs/EPIC-KITCHENS/RSPNET/32x112x112_R18_K400_LR0.0025.yaml
-bash diva_eval_action_detection.sh -c $cfg
+# echo "RSPNet"
+# cfg=$REPO/configs/EPIC-KITCHENS/RSPNET/32x112x112_R18_K400_LR0.0025.yaml
+# bash diva_eval_action_detection.sh -c $cfg
 
 echo "VideoMoCo"
 cfg=$REPO/configs/EPIC-KITCHENS/VIDEOMOCO/32x112x112_VMOCO_R18_K400_LR0.0025.yaml
